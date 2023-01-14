c5dfb3a85c93ed5703ea1da7bafa35cdfd82bfb6
R. Praveen Kumar <50480992+Praveen2135@users.noreply.github.com>
2023-01-14 08:45:00 +0530

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -8,9 +8,7 @@ import datetime as dt
 import streamlit as st
 #from tensorflow.keras.utils import HDF5Matrix
 
-if st.session_state['authentication_status'] == True or False:
-    pass
-else:
+if st.session_state == None:
     st.session_state['authentication_status'] = ""
 
 SD = Stocky_DB_2.StockyDb()
